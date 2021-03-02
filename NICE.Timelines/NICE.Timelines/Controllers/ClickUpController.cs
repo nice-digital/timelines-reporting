78867cce2edb68f4bf1e180c19dcdd200f885a59
John Williams <jcwilliams@gmail.com>
2021-03-02 15:31:23 +0000

    Adding an empty scaffolded api project for the timelines reporting spike. all the scaffolding here was created by a visual studio template.

---
+++ NICE.Timelines/NICE.Timelines/Controllers/ClickUpController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NICE.Timelines.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ClickUpController : ControllerBase
+	{
+		
+		private readonly ILogger<ClickUpController> _logger;
+
+		public ClickUpController(ILogger<ClickUpController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpPost]
+		public ActionResult Post()
+		{
+			return Ok();
+		}
+	}
+}
